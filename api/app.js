34cd10d3b812f45d7dce93e9385d552409d2a90f
Vladymyr Logvinov <vladymyrlogvinov@MacBook-Air-Vladymyr.local>
2022-02-18 13:55:33 +0200

    add basic middlewares

--- api/app.js
+++ api/app.js
@@ -7,10 +7,35 @@ const metricsRoutes = require('./routes/metrics');
 
 const app = express();
 
+connectDB();
+
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 
-connectDB();
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
+    next();
+ });
+
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+      error: {
+        message: error.message,
+      },
+    });
+});
+
+
 
 app.use('/metrics', metricsRoutes);
 
--- api/connectDB.js
+++ api/connectDB.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 const MONGO_URI = 'mongodb+srv://trendyol:tm3b0S9iNzgyephG@cluster0.1upom.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 async function connectDB() {
-  mongoose.connect(MONGO_URI, {useNewUrlParser: true});
-  const db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'connection error:'));
-  db.once('open', function() {
-    console.log('DB connected!')
-  });
+    try {
+        await mongoose.connect(MONGO_URI, {useNewUrlParser: true});
+        console.log('DB connected');
+    } catch(e) {
+        console.error(e.message);
+        process.exit(1);
+    }
 }
 
 module.exports = connectDB;
